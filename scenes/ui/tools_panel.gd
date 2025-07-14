df415a8112f7439113a8a1a8e1f40d2ae054d446
lilyniu88 <lilyniu86@gmail.com>
2025-07-14 15:01:43 -0400

    work on dialogue and guide

--- scenes/ui/tools_panel.gd
+++ scenes/ui/tools_panel.gd
@@ -14,6 +14,21 @@ extends PanelContainer
 	tool_tomato,
 ]
 
+func _ready() -> void:
+	ToolManager.enable_tool.connect(on_enable_tool_button)
+	
+	tool_tilling.disabled = true
+	tool_tilling.focus_mode = Control.FOCUS_NONE
+	
+	tool_watering_can.disabled = true
+	tool_watering_can.focus_mode = Control.FOCUS_NONE
+	
+	tool_corn.disabled = true
+	tool_corn.focus_mode = Control.FOCUS_NONE
+	
+	tool_tomato.disabled = true
+	tool_tomato.focus_mode = Control.FOCUS_NONE
+
 func _on_tool_axe_pressed() -> void:
 	ToolManager.select_tools(DataTypes.Tools.AxeWood)
 
@@ -35,3 +50,17 @@ func _process(_delta):
 		for b in tool_buttons:
 			b.set_pressed(false) 
 			b.release_focus()
+
+func on_enable_tool_button(tool: DataTypes.Tools) -> void:
+	if tool == DataTypes.Tools.TillGround:
+		tool_tilling.disabled = false
+		tool_tilling.focus_mode = Control.FOCUS_ALL
+	elif tool == DataTypes.Tools.WaterCrops:
+		tool_watering_can.disabled = false
+		tool_watering_can.focus_mode = Control.FOCUS_ALL
+	elif tool == DataTypes.Tools.PlantCorn:
+		tool_corn.disabled = false
+		tool_corn.focus_mode = Control.FOCUS_ALL
+	elif tool == DataTypes.Tools.PlantTomato:
+		tool_tomato.disabled = false
+		tool_tomato.focus_mode = Control.FOCUS_ALL
