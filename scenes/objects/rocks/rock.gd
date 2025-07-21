a2bc318fbedeaae4c167add098d96f90f6a6896f
lilyniu88 <lilyniu86@gmail.com>
2025-07-21 15:58:37 -0400

    added mineable rocks

--- scenes/objects/rocks/rock.gd
+++ scenes/objects/rocks/rock.gd
@@ -3,7 +3,7 @@ extends Sprite2D
 @onready var hurt_component: HurtComponent = $HurtComponent
 @onready var damage_component: DamageComponent = $DamageComponent
 
-var stone_scene = preload("res://scenes/objects/trees/log.tscn")
+var stone_scene = preload("res://scenes/objects/rocks/stone.tscn")
 
 func _ready() -> void:
 	hurt_component.hurt.connect(on_hurt)
@@ -11,12 +11,15 @@ func _ready() -> void:
 
 func on_hurt(hit_damage: int) -> void:
 	damage_component.apply_damage(hit_damage)
+	material.set_shader_parameter("shake_intensity", 0.3)
+	await get_tree().create_timer(0.5).timeout
+	material.set_shader_parameter("shake_intensity", 0.0)
 
 func on_max_damage_reached() -> void:
 	call_deferred("add_stone_scene")
 	queue_free()
 
 func add_stone_scene() -> void:
-	var log_instance = log_scene.instantiate() as Node2D
-	log_instance.global_position = global_position
-	get_parent().add_child(log_instance)
+	var stone_instance = stone_scene.instantiate() as Node2D
+	stone_instance.global_position = global_position
+	get_parent().add_child(stone_instance)
